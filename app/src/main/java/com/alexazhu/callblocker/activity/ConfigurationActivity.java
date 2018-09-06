fc4f8aa23d89e20f15f53fb5d7ecb4041e1a1b4e
Alex Zhu <alex.t.zhu@gmail.com>
2018-09-05 21:35:09 -0700

    Fix button color

--- app/src/main/java/com/alexazhu/callblocker/activity/ConfigurationActivity.java
+++ app/src/main/java/com/alexazhu/callblocker/activity/ConfigurationActivity.java
@@ -158,7 +158,6 @@ public class ConfigurationActivity extends AppCompatActivity {
     @Override
     public boolean onTouchEvent(MotionEvent event) {
         hideActionButtons();
-
         return true;
     }
 
